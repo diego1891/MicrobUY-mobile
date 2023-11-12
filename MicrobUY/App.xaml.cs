2edb1c60b4dbeaa1db84dbd860d7ddf107b03ebf
diego1891 <diegobronc1508@gmail.com>
2023-11-12 13:30:45 -0300

    Comienzo del proyecto

---
+++ MicrobUY/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MicrobUY
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
